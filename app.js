999d159d7f19837c597e4576baf6f8ee7988eb33
John Huang <little78926@gmail.com>
2016-04-18 14:27:01 -0700

    Add root get.

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use(express.static('./public'));
 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+})
+
 app.listen(3000, function () {
   console.log('server on 3000');
 })
