eb520da24c8e55891ea6eb4a12c31e774a79e1fc
John Huang <little78926@gmail.com>
2016-04-25 16:25:12 -0700

    Add login field to homepage and also change the layout of the brands showing. #6

--- public/home/home.controller.js
+++ public/home/home.controller.js
@@ -6,8 +6,8 @@ function home($http, listService) {
   vm.showRetailers = function () {
     var retailers = listService.getRetailers();
     retailers.then(function (res) {
+      vm.showed=true;
       vm.retailers = res.data;
-      console.log(vm.retailers);
     })
   }
   function activate() {
