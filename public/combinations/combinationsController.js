6148d18eb8b9e8d7972519b9ea3617a7e1412767
John Huang <little78926@gmail.com>
2016-04-29 12:29:26 -0700

    I add the scrollable function.#13

--- public/combinations/combinationsController.js
+++ public/combinations/combinationsController.js
@@ -5,47 +5,60 @@ app.$inject = ['$http', '$scope', '$location', 'userService', '$sce', 'collectio
 function combinations($http, $scope, $location, userService, $sce, $rootScope, collectionsService) {
   var vm = this;
   $scope._ = _;
-  // $scope.dragOptions = {
-  //   start: function(e) {
-  //     console.log("STARTING");
-  //   },
-  //   drag: function(e) {
-  //     console.log("DRAGGING");
-  //   },
-  //   stop: function(e) {
-  //     console.log("STOPPING");
-  //   },
-  //   container: 'comb-top-field',
-  // }
 
-  $scope.lastTop = function (array) {
-    if (vm.queryTop === 0 ) {
-      vm.queryTop = array.length - 1;
+  'use strict';
+
+  $scope.posX = 0;
+  $scope.posY = 0;
+
+  $scope.moveX = function (pixels) {
+    $scope.posX = $scope.posX + pixels;
+  };
+  $scope.moveY = function (pixels) {
+    $scope.posY = $scope.posY + pixels;
+  };
+  $scope.$evalAsync(function () {
+    $scope.$broadcast('content.changed', 1000);
+  });
+
+  $scope.center = function () {
+    $scope.posX = 600;
+    $scope.posY = 410;
+  };
+
+  $scope.last = function (array, location, index) {
+    if (index === 0 ) {
+      var target = array.length - 1;
     } else {
-      vm.queryTop = vm.queryTop - 1;
+      var target = index - 1;
+    }
+    if (location === 'top') {
+      vm.queryTop = target;
+    }
+    if (location === 'bot') {
+      vm.queryBot = target;
+    }
+    if (location === 'fullbody') {
+      vm.queryFullbody = target;
     }
   }
-  $scope.nextTop = function (array) {
-    if ((vm.queryTop + 1) === array.length) {
-      vm.queryTop = 0;
+  $scope.next = function (array, location, index) {
+    if ((index + 1) === array.length) {
+      var target = 0;
     } else {
-      vm.queryTop = vm.queryTop+1;
+      var target = index + 1;
+    }
+    if (location === 'top') {
+      vm.queryTop = target;
+    }
+    if (location === 'bot') {
+      vm.queryBot = target;
+    }
+    if (location === 'fullbody') {
+      vm.queryFullbody = target;
     }
   }
-  $scope.lastBot = function (array) {
-    if (vm.queryBot === 0 ) {
-      vm.queryBot = array.length - 1;
-    } else {
-      vm.queryBot = vm.queryBot - 1;
-    }
-  }
-  $scope.nextBot = function (array) {
-    if ((vm.queryBot + 1) === array.length) {
-      vm.queryBot = 0;
-    } else {
-      vm.queryBot = vm.queryBot+1;
-    }
-  }
+
 
   vm.newComb = function () {
     var json ={
@@ -88,120 +101,10 @@ function combinations($http, $scope, $location, userService, $sce, $rootScope, c
     vm.getCollectionsOf('fullbody');
   }
   activate();
-
 }
-
 
 app.directive('maker', function () {
   return {
     templateUrl: 'combinations/maker.html'
   }
 })
-
-// app.directive('draggable', ['$document' , function($document) {
-//     return {
-//       restrict: 'A',
-//       link: function(scope, elm, attrs) {
-//         var startX, startY, initialMouseX, initialMouseY;
-//         elm.css({position: 'absolute'});
-//
-//         elm.bind('mousedown', function($event) {
-//           startX = elm.prop('offsetLeft');
-//           startY = elm.prop('offsetTop');
-//           initialMouseX = $event.clientX;
-//           initialMouseY = $event.clientY;
-//           $document.bind('mousemove', mousemove);
-//           $document.bind('mouseup', mouseup);
-//           return false;
-//         });
-//
-//         function mousemove($event) {
-//           var dx = $event.clientX - initialMouseX;
-//           var dy = $event.clientY - initialMouseY;
-//           elm.css({
-//             top:  startY + dy + 'px',
-//             left: startX + dx + 'px'
-//           });
-//           return false;
-//         }
-//
-//         function mouseup() {
-//           $document.unbind('mousemove', mousemove);
-//           $document.unbind('mouseup', mouseup);
-//         }
-//       }
-//     };
-//   }]);
-//
-// app.directive('ngDraggable', function($document) {
-//   return {
-//     restrict: 'A',
-//     scope: {
-//       dragOptions: '=ngDraggable'
-//     },
-//     link: function(scope, elem, attr) {
-//       var startX, startY, x = 0, y = 0,
-//           start, stop, drag, container;
-//
-//       var width  = elem[0].offsetWidth,
-//           height = elem[0].offsetHeight;
-//
-//       // Obtain drag options
-//       if (scope.dragOptions) {
-//         start  = scope.dragOptions.start;
-//         drag   = scope.dragOptions.drag;
-//         stop   = scope.dragOptions.stop;
-//         var id = scope.dragOptions.container;
-//         if (id) {
-//           container = document.getElementById(id).getBoundingClientRect();
-//         }
-//       }
-//
-//       // Bind mousedown event
-//       elem.on('mousedown', function(e) {
-//         e.preventDefault();
-//         startX = e.clientX - elem[0].offsetLeft;
-//         startY = e.clientY - elem[0].offsetTop;
-//         $document.on('mousemove', mousemove);
-//         $document.on('mouseup', mouseup);
-//         if (start) start(e);
-//       });
-//
-//       // Handle drag event
-//       function mousemove(e) {
-//         y = e.clientY - startY;
-//         x = e.clientX - startX;
-//         setPosition();
-//         if (drag) drag(e);
-//       }
-//
-//       // Unbind drag events
-//       function mouseup(e) {
-//         $document.unbind('mousemove', mousemove);
-//         $document.unbind('mouseup', mouseup);
-//         if (stop) stop(e);
-//       }
-//
-//       // Move element, within container if provided
-//       function setPosition() {
-//         if (container) {
-//           if (x < container.left) {
-//             x = container.left;
-//           } else if (x > container.right - width) {
-//             x = container.right - width;
-//           }
-//           if (y < container.top) {
-//             y = container.top;
-//           } else if (y > container.bottom - height) {
-//             y = container.bottom - height;
-//           }
-//         }
-//
-//         elem.css({
-//           top: y + 'px',
-//           left:  x + 'px'
-//         });
-//       }
-//     }
-//   }
-// })
